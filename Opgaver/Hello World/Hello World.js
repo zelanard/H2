c89d2238363e1c95a9a9d35b1da193d2524ee2c4
Jonas Offersen <72074478+zelanard@users.noreply.github.com>
2024-04-15 10:24:35 +0200

    Changed method by which the message is displayed

--- Opgaver/Hello World/Hello World.js
+++ Opgaver/Hello World/Hello World.js
@@ -18,13 +18,30 @@ Body.appendChild(button);
 
 //create the text
 const display = document.createElement("p");
-display.classList.add("hidden");
-const text = document.createTextNode("Hello World");
-display.appendChild(text);
+//display.classList.add("hidden");
+const innerText = "Hello World!";
+display.innerHTML = innerText;
 Body.appendChild(display);
 
-
+/** adds or removes the text content of the display element.
+ * 
+ */
 function buttonClick() {
+	/** this alternative method does not append the code
+	 * intead it simply adds a class to it.
+	 * in the css file you can see that this class contains "Display: None".
+	 * this simply hides the element.
+	 * 
+	 * if (display.innerHTML == innerText) {
+	 * 	display.innerHTML = "";
+	 * } else {
+	 * 	display.innerHTML = innerText;
+	 * }
+	 * 
+	 * I would prefer this method, 
+	 * if i want the user to easily see the elemetns in the html inspector.
+	 */
+
 	if (display.classList.contains("hidden")) {
 		display.classList.remove("hidden");
 	} else {
